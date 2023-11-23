25e797a07188a519593293f0a0544c1bb5aee21f
Anne Margalith <annem6@illinois.edu>
2023-11-23 17:19:45 -0600

    Final Project

--- scripts/profile.py
+++ scripts/profile.py
@@ -2,3 +2,11 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 df = pd.read_csv('data/iris/iris.data')
+
+from ydata_profiling import ProfileReport
+
+# Generate the profiling report
+profile = ProfileReport(df, title='Profiling Report')
+
+# Write the report to an HTML file
+profile.to_file("profiling/report.html")
